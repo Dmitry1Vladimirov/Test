43b57de2e27e6e0fad85cf64843d3c40df3ae225
Dmitry <72342891+Dmitry1Vladimirov@users.noreply.github.com>
2022-10-30 14:36:38 +0300

    Update MainCorrected2.java

--- MainCorrected2.java
+++ MainCorrected2.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.util.Objects;
 import java.util.Scanner;
 
-public class Main {
+public class MainCorrected2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println(calc(scanner.nextLine()));
